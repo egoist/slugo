82132c27db1c38a0748d8ae13f63026f827a362c
EGOIST <0x142857@gmail.com>
2018-10-04 21:39:34 +0800

    replace long dash with two short dashes, fixed #3

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ export default function (input) {
     .replace(/<(?:.|\n)*?>/gm, '')
     // Remove special characters
     .replace(/[!\"#$%&'\(\)\*\+,\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '') // eslint-disable-line no-useless-escape
-    // Replace dots and spaces with a separator
+    // Replace dots and spaces with a short dash
     .replace(/(\s|\.)/g, '-')
+    // Replace long dash with two short dashes
+    .replace(/—/g, '--')
     // Make the whole thing lowercase
     .toLowerCase()
 }
